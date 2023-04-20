c29be9d2648fd8fb4097eadf00b06e220a3c1790
Abiola Martins <hsemartins@gmail.com>
2023-04-20 15:29:50 +0300

    Added index.html file, style.css file and script.js file.

---
+++ working.js
@@ -0,0 +1,35 @@
+'use strict';
+//Before refactoring the codes.
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button Clicked.');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+  const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  };
+
+  btnCloseModal.addEventListener(
+    'click',
+    closeModal
+    // modal.classList.add('hidden');
+    // overlay.classList.add('hidden');
+  );
+
+  overlay.addEventListener(
+    'click',
+    closeModal
+    // modal.classList.add('hidden');
+    // overlay.classList.add('hidden');
+  );
+}
